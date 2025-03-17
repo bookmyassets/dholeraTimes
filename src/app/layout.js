699e643e957773928a797f9b25b08b3054ddf6f6
bookmyassets <bookmyassets101@gmail.com>
2025-03-17 16:39:34 +0530

    fav

--- src/app/layout.js
+++ src/app/layout.js
@@ -156,6 +156,24 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="w4B8pqZZDySMLUmxZYsGxeKSCsTI_aHk-myN3iKS3CU"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
