7d1b7b8a31f7100292f2ae51802efbbdad55c028
bookmyassets <bookmyassets101@gmail.com>
2025-03-17 16:18:54 +0530

    videos

---
+++ src/app/Infopack/videos/page.js
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import { getblogs } from "@/sanity/lib/api"
+import { urlFor } from "@/sanity/lib/image"
+import Image from "next/image"
+import { CalendarDays, MessageSquare, User } from "lucide-react"
+
+export default async function BlogPage() {
+
+
+  return (
+    <div>
+        <div className="text-center pt-20 md:text-5xl text-2xl font-bold">
+            Videos
+        </div>
+    </div>
+  )
+}
+
+
+ 
