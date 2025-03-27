3dbbdea0d5c749e32cc9c4e9804c414145ee3eba
bookmyassets <bookmyassets101@gmail.com>
2025-03-27 17:20:07 +0530

    analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -277,6 +277,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7TB2TDXYX0"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-7TB2TDXYX0');
+</script>
         <meta
           name="google-site-verification"
           content="w4B8pqZZDySMLUmxZYsGxeKSCsTI_aHk-myN3iKS3CU"
