ac4ff27c40aab2ff74178cb0ccfd201dd741c1e7
bookmyassets <bookmyassets101@gmail.com>
2025-03-28 12:03:12 +0530

    new video uploaded

--- src/app/infopack/videos/page.js
+++ src/app/infopack/videos/page.js
@@ -5,7 +5,7 @@ export default async function BlogPage() {
   const videos = [
     "hNbWaEU1d_A?si=c5c7r-9oUr-CB8QD",
     "TwsGjPP-wPg?si=9zL4ChmD54Iun750",
-    "hNbWaEU1d_A?si=c5c7r-9oUr-CB8QD",
+    "Ac0wGh72S2g?si=pzZvS9Y3bKx2mOnL",
     "hNbWaEU1d_A?si=c5c7r-9oUr-CB8QD",
   ]; // YouTube video IDs
 
