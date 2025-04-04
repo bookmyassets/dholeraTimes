2199ea27b8433ccae78f9d2dd73dfb3163c4a6d3
bookmyassets <bookmyassets101@gmail.com>
2025-04-04 15:42:19 +0530

    bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -554,7 +554,7 @@ Exclusive residential plots in Dholera Smart City Gujarat! Close to Dholera SIR 
                       onClick={toggleBlogsDropdown}
                       className="text-white hover:text-orange-200 px-3 py-2 flex items-center gap-1"
                     >
-                      <Link href="/Dholera-Updates">Dholera Update</Link>
+                      <Link href="/Dholera-Updates/blogs">Dholera Update</Link>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         className={`h-4 w-4 text-white transition-transform duration-300 ${
