b6eacba903bea8f81cec73542831eaf8118018dd
bookmyassets <bookmyassets101@gmail.com>
2025-04-14 11:27:04 +0530

    redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,25 @@ const nextConfig = {
     images: {
       domains: ['cdn.sanity.io'], // Allow Sanity images
     },
+    async redirects() {
+      return [
+        {
+          source: '/INFOPACK',
+          destination: '/infopack',
+          permanent: true,
+        },
+        {
+          source: '/Infopack',
+          destination: '/infopack',
+          permanent: true,
+        },
+        {
+          source: '/INFOpack',
+          destination: '/infopack',
+          permanent: true,
+        },
+      ];
+    },
   };
   
 export default nextConfig;
