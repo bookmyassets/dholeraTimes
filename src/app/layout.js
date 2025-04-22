4ebf0c4e8e691e0a70999c7e9b8a5d9445fd8d73
bookmyassets <bookmyassets101@gmail.com>
2025-04-22 19:34:24 +0530

    font style cvhanged

--- src/app/layout.js
+++ src/app/layout.js
@@ -277,6 +277,13 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <style jsx global>{`
+        @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap");
+
+        body {
+          font-family: "Poppins", sans-serif;
+        }
+      `}</style>
       <head>
         <Script
           strategy="afterInteractive"
