8c40af5df74664bed4aa71fb77fd8d0031a2bd71
bookmyassets <bookmyassets101@gmail.com>
2025-05-02 15:57:54 +0530

    gtm whatsapp

--- src/app/(landing)/dholera-times-plots/layout.js
+++ src/app/(landing)/dholera-times-plots/layout.js
@@ -197,7 +197,7 @@ twq('config','oxi2l');
         </noscript> */}
 
         <div>{children}</div>
-        <FloatingIcons />
+        <FloatingIcons  id="whatsapp" />
         <Footer />
       </body>
     </html>
