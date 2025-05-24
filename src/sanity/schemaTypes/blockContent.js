402083112f0275af992a73e6e9b6f1301eb7c52e
bookmyassets <bookmyassets101@gmail.com>
2025-05-24 14:26:05 +0530

    changes

--- src/sanity/schemaTypes/blockContent.js
+++ src/sanity/schemaTypes/blockContent.js
@@ -1,44 +1,64 @@
-// sanity/schemaTypes/blockContent.js
 export default {
-    name: 'blockContent',
-    title: 'Block Content',
-    type: 'array',
-    of: [
-      {
-        title: 'Block',
-        type: 'block',
-        styles: [
-          { title: 'Normal', value: 'normal' },
-          { title: 'H1', value: 'h1' },
-          { title: 'H2', value: 'h2' },
-          { title: 'H3', value: 'h3' },
-          { title: 'Quote', value: 'blockquote' },
+  name: 'blockContent',
+  title: 'Block Content',
+  type: 'array',
+  of: [
+    {
+      title: 'Block',
+      type: 'block',
+      // Styles including text alignment
+      styles: [
+        { title: 'Normal', value: 'normal' },
+        { title: 'H1', value: 'h1' },
+        { title: 'H2', value: 'h2' },
+        { title: 'H3', value: 'h3' },
+        { title: 'H4', value: 'h4' },
+        { title: 'H5', value: 'h5' },
+        { title: 'H6', value: 'h6' },
+        { title: 'Quote', value: 'blockquote' },
+        { title: 'Left Align', value: 'leftAlign' },
+        { title: 'Center Align', value: 'centerAlign' },
+        { title: 'Right Align', value: 'rightAlign' },
+        { title: 'Justify', value: 'justify' },
+      ],
+      lists: [
+        { title: 'Bullet', value: 'bullet' },
+        { title: 'Numbered', value: 'number' }
+      ],
+      marks: {
+        decorators: [
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' },
+          { title: 'Underline', value: 'underline' },
         ],
-        lists: [{ title: 'Bullet', value: 'bullet' }],
-        marks: {
-          decorators: [
-            { title: 'Strong', value: 'strong' },
-            { title: 'Emphasis', value: 'em' },
-          ],
-          annotations: [
-            {
-              name: 'link',
-              title: 'Link',
-              type: 'object',
-              fields: [
-                {
-                  name: 'href',
-                  title: 'URL',
-                  type: 'url',
-                },
-              ],
-            },
-          ],
-        },
+        annotations: [
+          {
+            name: 'link',
+            title: 'Link',
+            type: 'object',
+            fields: [
+              {
+                name: 'href',
+                title: 'URL',
+                type: 'url',
+              },
+            ],
+          },
+        ],
       },
-      {
-        type: 'image',
-        options: { hotspot: true },
-      },
-    ],
-  };
+    },
+    {
+      type: 'image',
+      options: { hotspot: true },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative Text',
+        }
+      ]
+    },
+    // Add code block type
+  
+  ],
+};
