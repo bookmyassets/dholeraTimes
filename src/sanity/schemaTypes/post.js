74f5c1bcaf69625b2267bf045c248f002eccd8dd
bookmyassets <bookmyassets101@gmail.com>
2025-06-17 16:14:24 +0530

    revrt bck

--- src/sanity/schemaTypes/post.js
+++ src/sanity/schemaTypes/post.js
@@ -1,93 +1,81 @@
 // sanity/schemaTypes/post.js
 export default {
-  name: "post",
-  title: "Post",
-  type: "document",
-  fields: [
-    {
-      name: "title",
-      title: "Title",
-      type: "string",
-    },
-    {
-      name: "metaTitle",
-      title: "Meta Title",
-      type: "string",
-    },
-    {
-      name: "metaDescription",
-      title: "Meta Description",
-      type: "string",
-    },
-    {
-      name: "keywords",
-      title: "Meta Keywords",
-      type: "array",
-      of: [{ type: "string" }],
-    },
-    {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
-      options: {
-        source: "title",
-        maxLength: 96,
+    name: 'post',
+    title: 'Post',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
       },
-    },
-    {
-      name: "author",
-      title: "Author",
-      type: "reference",
-      to: [{ type: "author" }],
-    },
-    {
-      name: "mainImage",
-      title: "Main Image",
-      type: "object",
-      fields: [
-        {
-          name: "image",
-          title: "Image",
-          type: "image",
-          options: {
-            hotspot: true,
-          },
-        },
-        {
-          name: "url",
-          type: "url",
-          title: "Link URL",
-        },
-      ],
-    },
-    {
-      name: "categories",
-      title: "Categories",
-      type: "array",
-      of: [{ type: "reference", to: { type: "category" } }],
-    },
-    {
-      name: "publishedAt",
-      title: "Published At",
-      type: "datetime",
-    },
-    {
-      name: "Location",
-      title: "Location",
-      type: "string",
-    },
-    {
-      name: "body",
-      title: "Body",
-      type: "blockContent",
-    },
-    {
-      name: "pdfFile",
-      title: "PDF File",
-      type: "file",
-      options: {
-        accept: ".pdf", // Ensures only PDFs can be uploaded
+      {
+        name: 'metaTitle',
+        title: 'Meta Title',
+        type: 'string',
       },
-    },
-  ],
-};
+      {
+        name: 'metaDescription',
+        title: 'Meta Description',
+        type: 'string',
+      },
+      {
+        name: 'keywords',
+        title: 'Meta Keywords',
+        type: 'array',
+        of: [{ type: 'string' }],
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'title',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'author',
+        title: 'Author',
+        type: 'reference',
+        to: [{ type: 'author' }],
+      },
+      {
+        name: 'mainImage',
+        title: 'Main Image',
+        type: 'image',
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: 'categories',
+        title: 'Categories',
+        type: 'array',
+        of: [{ type: 'reference', to: { type: 'category' } }],
+      },
+      {
+        name: 'publishedAt',
+        title: 'Published At',
+        type: 'datetime',
+      },
+      {
+        name: 'Location',
+        title: 'Location',
+        type: 'string',
+      },
+      {
+        name: 'body',
+        title: 'Body',
+        type: 'blockContent',
+      },
+      {
+        name: 'pdfFile',
+        title: 'PDF File',
+        type: 'file',
+        options: {
+          accept: '.pdf', // Ensures only PDFs can be uploaded
+        },
+      },
+    ],
+  };
