f320b7465705378a47237d6a5b4a389479b67a34
bookmyassets <bookmyassets101@gmail.com>
2025-06-30 12:38:47 +0530

    chnages

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -6,8 +6,8 @@ export default function page() {
   const [showForm, setShowForm] = useState(false);
   return (
     <>
-    <link ref="canonical" href='https://www.dholeratimes.com' />
       <title>Dholera Smart City Gujarat | High ROI Plots -Dholera Times</title>
+    
       <meta name='description' content='Exclusive residential plots in Dholera Smart City Gujarat! Close to Dholera SIR & International Airport. Book now for high returns!' />
         <div>
             <NewHome openForm={() => setShowForm(true)} />
