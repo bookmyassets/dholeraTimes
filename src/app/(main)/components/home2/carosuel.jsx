38c04f6642abe8294a42c7a0721816ae5bdc1391
bookmyassets <bookmyassets101@gmail.com>
2025-09-04 14:06:00 +0530

    changes

--- src/app/(main)/components/home2/carosuel.jsx
+++ src/app/(main)/components/home2/carosuel.jsx
@@ -7,10 +7,10 @@ import { AnimatePresence, motion } from "framer-motion";
 /* image import */
 import hero1 from "@/assets/dt-home-banner.webp";
 import hero2 from "@/assets/dt-westwyn-county-home-banner.webp";
-import hero2 from "@/assets/DT-banner3.webp";
+import hero3 from "@/assets/DT-banner3.webp";
 import heroM1 from "@/assets/DTbanner1_mobile.webp";
 import heroM2 from "@/assets/DTbanner2_mobile.webp";
-import heroM2 from "@/assets/DTbanner3_mobile.webp";
+import heroM3 from "@/assets/DTbanner3_mobile.webp";
 import BrochureForm from "../BrochureForm";
 
 export default function HOME2() {
@@ -48,6 +48,11 @@ export default function HOME2() {
       desktop: hero2,
       tablet: hero2,
       mobile: heroM2,
+    },
+    {
+      desktop: hero3,
+      tablet: hero3,
+      mobile: heroM3,
     },
   ];
 
