4889c66bebd95dd55a556a4e3e63f8971d5228aa
bookmyassets <bookmyassets101@gmail.com>
2025-09-26 16:04:36 +0530

    changes

--- src/app/(main)/dholera-residential-plots/westwyn-estate/Hero.jsx
+++ src/app/(main)/dholera-residential-plots/westwyn-estate/Hero.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import wc from "@/assets/residential/westwyn-estate.webp";
+import wc from "@/assets/residential/westwyn-estate2.webp";
 import Link from "next/link";
 import { AnimatePresence } from "framer-motion";
 import BrochureDownload from "../../components/BrochureDownload";
