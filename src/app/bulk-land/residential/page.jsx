2af901447307f010cd7dbd943d8b2ee2c5e75a5b
bookmyassets <bookmyassets101@gmail.com>
2026-01-14 16:15:34 +0530

    bulk land schema markups

--- src/app/bulk-land/residential/page.jsx
+++ src/app/bulk-land/residential/page.jsx
@@ -76,6 +76,33 @@ export default function page() {
 
   return (
     <>
+    <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Corporation",
+            name: "BookMyAssets",
+            alternateName: "BMA",
+            url: "https://www.dholeratimes.com/",
+            logo: "https://www.dholeratimes.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fdt.7009f759.webp&amp;w=256&amp;q=75",
+            address: {
+              "@type": "PostalAddress",
+              streetAddress: "620, JMD Megapolis, Sohna Rd, Sector 48,",
+              addressLocality: "Gurgaon",
+              addressRegion: "Haryana",
+              postalCode: "122001",
+              addressCountry: "IN",
+            },
+            sameAs: [
+              "https://www.facebook.com/profile.php?id=61572970112485",
+              "https://www.instagram.com/dholeratimes/",
+              "https://www.youtube.com/@dholeratimes",
+              "https://x.com/dholeratimes",
+            ],
+          }),
+        }}
+      />
       <title>Residential Bulk Land in Dholera | Smart City Plots</title>
       <meta
         name="description"
