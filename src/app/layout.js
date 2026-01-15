9d96d8ad07ec1195af86509aa3006e637ba6d4c4
bookmyassets <bookmyassets101@gmail.com>
2026-01-15 12:53:11 +0530

    gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -153,13 +153,23 @@ export default function RootLayout({ children }) {
 
         <Script type="text/javascript">
           {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          
+           `}
+        </Script>
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-W7WP6QXX');
-           `}
-        </Script>
+            `,
+          }}
+        />
 
         <Script type="text/javascript" strategy="afterInteractive">
           {`
