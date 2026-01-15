e8bb0291dc3761cbc9c97500e6b9af4bf4f870e7
bookmyassets <bookmyassets101@gmail.com>
2026-01-15 12:40:02 +0530

    gtm added

--- src/app/layout.js
+++ src/app/layout.js
@@ -151,6 +151,16 @@ export default function RootLayout({ children }) {
           }}
         />
 
+        <Script type="text/javascript">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W7WP6QXX');
+           `}
+        </Script>
+
         <Script type="text/javascript" strategy="afterInteractive">
           {`
               (function(c,l,a,r,i,t,y){
@@ -170,7 +180,7 @@ export default function RootLayout({ children }) {
         <ScrollToTop />
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NLL6M3PL"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W7WP6QXX"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
