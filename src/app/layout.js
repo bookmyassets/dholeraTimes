eb2fec599c9c59614caf680ee77f258ed7161ff3
bookmyassets <bookmyassets101@gmail.com>
2026-01-15 13:16:58 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -151,20 +151,6 @@ export default function RootLayout({ children }) {
           }}
         />
 
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W7WP6QXX');
-            `,
-          }}
-        />
-
         <Script type="text/javascript" strategy="afterInteractive">
           {`
               (function(c,l,a,r,i,t,y){
@@ -184,7 +170,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <ScrollToTop />
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W7WP6QXX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NLL6M3PL"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
