35867d7fc172a09e5421e7910cb93102eaf58b36
bookmyassets <bookmyassets101@gmail.com>
2026-01-15 16:10:09 +0530

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -152,19 +152,6 @@ export default function RootLayout({ children }) {
           }}
         />
         
-        {/* <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || []; 
-              function gtag(){ dataLayer.push(arguments); }
-              gtag('js', new Date());
-              gtag('config', 'G-7TB2TDXYX0'); 
-            `,
-          }}
-        /> */}
-
         <Script
           id="google-analytics"
           strategy="afterInteractive"
